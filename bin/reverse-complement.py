1146a4b786b0103f83ede0f3653b933e6419a714
Will Trimble <trimble@anl.gov>
2017-03-06 11:21:33 -0600

    specify python 2.7

--- bin/reverse-complement.py
+++ bin/reverse-complement.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 # Generates reverse complements of a column in a delimited table, and appends the reverse
 # complement as the last column of the table. 
