010161c547624c9570e318fa23f773af4e6437b9
Andreas Wilke <wilke@anl.gov>
2023-08-22 14:30:39 -0500

    Changed name of base image

--- Docker/build-anl-seq-service.sh
+++ Docker/build-anl-seq-service.sh
@@ -1,12 +1,12 @@
 
 TAG=0.1
 
-docker buildx build --platform linux/arm64 -t wilke/anl-seq-service:arm64 -f Docker/Base.dockerfile .
-docker buildx build --platform linux/amd64 -t wilke/anl-seq-service:amd64 -f Docker/Base.dockerfile .
+docker buildx build --platform linux/arm64 -t wilke/anl-seq-service:arm64 -f Docker/base.dockerfile .
+docker buildx build --platform linux/amd64 -t wilke/anl-seq-service:amd64 -f Docker/base.dockerfile .
 docker push wilke/anl-seq-service:arm64
 docker push wilke/anl-seq-service:amd64
-docker manifest create --amend wilke/anl-seq-service:${TAG} wilke/wilke/anl-seq-service:amd64 wilke/wilke/anl-seq-service:arm64 
-docker manifest create --amend wilke/anl-seq-service:latest wilke/wilke/anl-seq-service:amd64 wilke/wilke/anl-seq-service:arm64 
+docker manifest create --amend wilke/anl-seq-service:${TAG} wilke/anl-seq-service:amd64 wilke/anl-seq-service:arm64 
+docker manifest create --amend wilke/anl-seq-service:latest wilke/anl-seq-service:amd64 wilke/anl-seq-service:arm64 
 
 docker manifest push wilke/anl-seq-service:${TAG}
 docker manifest push wilke/anl-seq-service:latest  
